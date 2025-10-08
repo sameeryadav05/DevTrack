7f0cc27511a6e940e41db33c5abdf993af66538a
Sameer yadav <sameer.manoj2005@gmail.com>
2025-10-08 10:54:42 +0530

    folder structure and configs

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const app = express();
+const port = process.env.PORT
+
+app.listen(port,()=>console.log('server us listening on port ',port))
